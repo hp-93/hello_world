5aee9baf1d73f7be69950db1814ca6ee5008753d
hp <hp1993star@foxmail.com>
2018-08-30 00:32:44 +0800

    clion project init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// 如何外部构建  http://sewm.pku.edu.cn/src/paradise/reference/CMake%20Practice.pdf
+// windows 下如何生成第三方库，并且成功的调用生成的库
+// windows 下如何链接外部第三方库如curl库
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
